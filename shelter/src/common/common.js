b7a211bc3293dc806a736b7ce34357a06d674975
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-08-17 15:18:57 +0300

    feat: add popup closure

--- shelter/src/common/common.js
+++ shelter/src/common/common.js
@@ -16,33 +16,55 @@ function handleBurger(body) {
         }
 
         document.querySelectorAll("#menu-list *").forEach((item) => {
-            item.addEventListener('click', () => hideMenu(menu, burger, body));
+            item.addEventListener('click', () => hideMenu(menu, burger));
         });
     }
 
-    menu.addEventListener('click', () => hideMenu(menu, burger, body));
+    menu.addEventListener('click', () => hideMenu(menu, burger));
 }
 
-function hideMenu(elem1, elem2, elem3) {
+function hideMenu(elem1, elem2) {
     elem1.classList.remove("active");
     elem2.classList.remove("active");
-    elem3.style.overflowY = "auto";
+    body.style.overflowY = "auto";
 }
 
-function openPopup() {
+function handlePopup() {
     const cards = document.querySelectorAll('.card');
+    const modal = document.querySelector('.modal-wrapper');
 
 
     cards.forEach(item => {
         item.onclick = () => {
-            const modal = document.querySelector('.modal-wrapper');
-            modal.style.opacity = '1';
-            modal.style.visibility = 'visible';
-
-            body.style.overflowY = "hidden";
+            openPopup(modal);
         }
     })
+
+    closePopup(modal);
+}
+
+function closePopup(modal) {
+    const closeBtn = document.querySelector('.close-btn');
+    closeBtn.addEventListener('click', () => hidePopup(modal));
+
+    modal.onclick = (e) => {
+        if (e.target === modal) {
+            hidePopup(modal);
+        }
+    }
+}
+
+function openPopup(modal) {
+    modal.style.opacity = '1';
+    modal.style.visibility = 'visible';
+    body.style.overflowY = "hidden";
+}
+
+function hidePopup(modal) {
+    modal.style.opacity = '0';
+    modal.style.visibility = 'hidden';
+    body.style.overflowY = "auto";
 }
 
 handleBurger(body);
-openPopup();
+handlePopup(); 
