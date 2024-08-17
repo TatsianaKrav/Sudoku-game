9f855ab8267d72d9412aed6488218df220840646
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-08-17 09:24:01 +0300

    fix: change burger menu position

--- shelter/src/common/common.js
+++ shelter/src/common/common.js
@@ -1,29 +1,30 @@
 function handleBurger() {
     const burger = document.getElementsByClassName('burger')[0];
     const menu = document.getElementsByClassName('menu')[0];
+    const body = document.getElementsByTagName("body")[0];
 
     burger.onclick = () => {
         burger.classList.toggle('active');
         menu.classList.toggle('active');
 
         if (burger.classList.contains("active")) {
-            document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+            body.style.overflowY = "hidden";
         } else {
-            document.getElementsByTagName("body")[0].style.overflowY = "auto";
+            body.style.overflowY = "auto";
         }
 
         document.querySelectorAll("#menu-list *").forEach((item) => {
-            item.addEventListener('click', () => hideMenu(menu, burger));
+            item.addEventListener('click', () => hideMenu(menu, burger, body));
         });
     }
 
-    menu.addEventListener('click', () => hideMenu(menu, burger));
+    menu.addEventListener('click', () => hideMenu(menu, burger, body));
 }
 
-function hideMenu(elem1, elem2) {
+function hideMenu(elem1, elem2, elem3) {
     elem1.classList.remove("active");
     elem2.classList.remove("active");
-    document.getElementsByTagName("body")[0].style.overflowY = "auto";
+    elem3.style.overflowY = "auto";
 }
 
 handleBurger();
