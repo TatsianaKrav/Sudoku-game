df81df3b19c3a21a2d90b010d4d441179da4c35d
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-08-25 11:30:49 +0300

    fix: change slider

--- shelter/src/common/common.js
+++ shelter/src/common/common.js
@@ -15,26 +15,42 @@ window.onload = () => {
 }
 
 const arrOfIndex = pets.map((item, index) => index);
+const body = document.getElementsByTagName("body")[0];
 
 function handleSlider() {
     const nextBtn = document.querySelector('.narrow-next');
     const backBtn = document.querySelector('.narrow-back');
     const sliders = document.querySelector('.pets-sliders');
 
-    nextBtn.onclick = () => {
-        sliders.style.animation = 'slide-right 0.7s linear';
-    }
-
-    backBtn.onclick = () => {
-        sliders.style.animation = 'slide-left 0.7s linear';
-    }
-
-    sliders.addEventListener('animationend', () => {
-        sliders.style.animation = 'none';
-    })
+    nextBtn.addEventListener('click', nextSlide);
+    backBtn.addEventListener('click', prevSlider);
 }
 
-const body = document.getElementsByTagName("body")[0];
+let countSlider = 1;
+let slidersList = document.querySelectorAll('.slider');
+
+function nextSlide() {
+    slidersList[countSlider].style.animation = "next1 0.5s linear forwards";
+    if (countSlider >= slidersList.length - 1) {
+        countSlider = 0;
+    } else {
+        countSlider++;
+    }
+
+    slidersList[countSlider].style.animation = "next2 0.5s linear forwards";
+}
+
+function prevSlider() {
+    slidersList[countSlider].style.animation = "prev1 0.5s linear forwards";
+    if (countSlider === 0) {
+        countSlider = slidersList.length - 1;
+    } else {
+        countSlider--;
+    }
+    slidersList[countSlider].style.animation = "prev2 0.5s linear forwards";
+}
+
+
 
 function goTo(e, link) {
     e.preventDefault();
