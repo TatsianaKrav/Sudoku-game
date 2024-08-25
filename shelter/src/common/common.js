581d7ed0d8fa20c343ee682547d78ce653a6ff9f
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-08-25 15:48:52 +0300

    feat: make slider unrepeatable

--- shelter/src/common/common.js
+++ shelter/src/common/common.js
@@ -30,6 +30,7 @@ let countSlider = 1;
 let slidersList = document.querySelectorAll('.slider');
 
 function nextSlide() {
+
     slidersList[countSlider].style.animation = "next1 0.5s linear forwards";
     if (countSlider >= slidersList.length - 1) {
         countSlider = 0;
@@ -38,7 +39,19 @@ function nextSlide() {
     }
 
     slidersList[countSlider].style.animation = "next2 0.5s linear forwards";
+
+    // recreate cards
+    let count = countSlider;
+    if (count++ >= slidersList.length - 1) {
+        rewriteCards(slidersList[0], slidersList[countSlider]);
+    } else {
+        rewriteCards(slidersList[count++], slidersList[countSlider]);
+    }
+
+    count = 0;
 }
+
+
 
 function prevSlider() {
     slidersList[countSlider].style.animation = "prev1 0.5s linear forwards";
@@ -48,6 +61,33 @@ function prevSlider() {
         countSlider--;
     }
     slidersList[countSlider].style.animation = "prev2 0.5s linear forwards";
+
+    // recreate cards
+    let count = countSlider;
+    if (count-- <= 0) {
+        rewriteCards(slidersList[slidersList.length - 1], slidersList[countSlider]);
+    } else {
+        rewriteCards(slidersList[count--], slidersList[countSlider]);
+    }
+
+    count = 0;
+}
+
+function rewriteCards(sliderToShow, currentSlider) {
+    console.log(currentSlider);
+    console.log(sliderToShow);
+    const cards = [...currentSlider.children];
+    const arrOfPetNames = cards.map(card => card.dataset.name);
+    const arrOfIndex = [];
+
+    pets.forEach((pet, index) => {
+        if (!arrOfPetNames.includes(pet.name)) {
+            arrOfIndex.push(index);
+        }
+    })
+
+    sliderToShow.innerHTML = '';
+    createCard(sliderToShow, shuffle(arrOfIndex).slice(0, 3));
 }
 
 
