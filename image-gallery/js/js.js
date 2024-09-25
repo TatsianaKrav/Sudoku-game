476b0a85d657f192a02141557692715beb4cfa70
Татьяна Кравченко <tats.krauchanka@gmail.com>
2024-09-25 15:16:16 +0300

    feat: add input handler

--- image-gallery/js/js.js
+++ image-gallery/js/js.js
@@ -1,16 +1,48 @@
 const KEY = 'Z7YhGgww0fM50XllRlscVwIAhaLF97wpYJj6uiPn3us';
-const url = `https://api.unsplash.com/photos/random?client_id=${KEY}&count=15&orientation=landscape`;
 
 const gallery = document.querySelector('.main');
 const form = document.querySelector('.search');
 const input = document.querySelector('.search-field');
+const magnifier = document.querySelector('.magnifier');
+const cross = document.querySelector('.cross');
+
+window.onload = () => {
+    input.focus();
+    input.addEventListener('input', () => {
+        if (input.value) {
+            setTimeout(() => {
+                magnifier.classList.remove('active');
+                cross.classList.add('active');
+            }, 1000)
+
+        } else {
+            setTimeout(() => {
+                magnifier.classList.add('active');
+                cross.classList.remove('active');
+            }, 1000)
+        }
+    })
+}
 
 async function getData() {
-    const response = await fetch(url);
-    const data = await response.json(); // массив объектов
-    console.log(data);
 
-    renderImages(data);
+    try {
+        const url = `https://api.unsplash.com/photos/random?client_id=${KEY}&count=15&orientation=landscape`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+            renderImages(data);
+        } else {
+            gallery.innerText = 'Something went wrong...';
+        }
+    } catch (error) {
+        console.log(error);
+        gallery.innerText = 'Something went wrong...';
+    }
+
 }
 
 getData();
@@ -31,3 +63,11 @@ function createImageWrapper() {
 }
 
 
+
+
+cross.addEventListener('click', () => {
+    cross.classList.remove('active');
+    magnifier.classList.add('active');
+    input.value = '';
+    input.focus();
+})
